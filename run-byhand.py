7abb16b77e11c373d5ec97e5195c9578cd4ac1b6
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-03-22 17:11:26 +0000

    TracerProblem class

--- run-byhand.py
+++ run-byhand.py
@@ -1,6 +1,6 @@
 from firedrake import *
 import math
-# op2.init(log_level=INFO)
+op2.init(log_level=INFO)
 
 from adapt.adaptivity import *
 from adapt.interpolation import interp
@@ -19,81 +19,109 @@ L = float(geo.readline().replace(';', '=').split('=')[1])
 geo.close()
 yt1=W/2
 yt2=W/2
+A_T = math.pi*(D/2)**2
 
-def solve_turbine(mesh2d, op=TurbineOptions()):
 
-    # physical parameters
-    b = Constant(40.)
-    nu = Constant(op.viscosity)
-    C_b = Constant(op.drag_coefficient)
-    g = Constant(9.81)
+class TurbineProblem():
 
-    # turbine parameters
-    D = 18
-    A_T = math.pi*(D/2)**2
-    correction = 4/(1+math.sqrt(1-A_T/(b*D)))**2
-    C_T = op.thrust_coefficient * correction
-    op.region_of_interest = [(xt1, yt1, D/2), (xt2, yt2, D/2)]
-    turbine_density = op.bump(mesh2d, scale=2./assemble(op.bump(mesh2d)*dx))
-    C_D = Constant(C_T * A_T / 2.) * turbine_density
+    def __init__(self, mesh=None, op=TurbineOptions()):
+        self.mesh = Mesh('channel.msh') if mesh is None else mesh
+        self.op = op
 
-    # function spaces etc
-    Taylor_Hood = VectorFunctionSpace(mesh2d, "CG", 2) * FunctionSpace(mesh2d, "CG", 1)
-    q = Function(Taylor_Hood)
-    u, eta = split(q)
-    z, zeta = TestFunctions(Taylor_Hood)
-    n = FacetNormal(mesh2d)
-    H = eta + b
-    u_old = interpolate(as_vector((3., 0.)), Taylor_Hood.sub(0))
+        # physical parameters
+        self.b = Constant(40.)
+        self.nu = Constant(op.viscosity)
+        self.C_b = Constant(op.drag_coefficient)
+        self.g = Constant(9.81)
 
-    F = 0
-    # advection term
-    F += inner(z, dot(u_old, nabla_grad(u)))*dx  # note use of u_old
-    # pressure gradient term
-    F += g*dot(z, grad(eta))*dx
-    # viscosity term
-    F += -nu*inner(z, dot(n, nabla_grad(u)))*ds
-    F += nu*inner(grad(z), grad(u))*dx
-    # drag term
-    F += (C_D+C_b)*sqrt(dot(u_old, u_old))*dot(z, u)/H*dx  # note use of u_old
-    # hudiv term (in.c Neumann condition)
-    F += zeta*H*dot(u, n)*ds(1) + zeta*H*dot(u, n)*ds(2)
-    F += -H*dot(u, grad(zeta))*dx
+        # turbine parameters
+        correction = 4/(1+math.sqrt(1-A_T/(self.b*D)))**2
+        C_T = op.thrust_coefficient * correction
+        self.op.region_of_interest = [(xt1, yt1, D/2), (xt2, yt2, D/2)]
+        turbine_density = self.op.bump(self.mesh, scale=2./assemble(self.op.bump(self.mesh)*dx))
+        self.C_D = Constant(C_T * A_T / 2.) * turbine_density
 
-    # Dirichlet boundary conditions
-    inflow = Constant((3., 0.))
-    bc1 = DirichletBC(Taylor_Hood.sub(0), inflow, 1)
-    bc2 = DirichletBC(Taylor_Hood.sub(1), 0, 2)
+        # Taylor-Hood space
+        self.V = VectorFunctionSpace(self.mesh, "CG", 2) * FunctionSpace(self.mesh, "CG", 1)
+        self.sol = Function(self.V)
+        self.n = FacetNormal(self.mesh)
 
-    # solve
-    params = {
-              'snes_type': 'newtonls',
-              #'ksp_type': 'gmres',
-              'ksp_type': 'preonly',
-              'pc_type': 'lu',
-              #'pc_factor_mat_solver_type': 'mumps',
-              'mat_type': 'aij',
-              'snes_monitor': None,
-              'ksp_monitor': None,
-              'ksp_converged_reason': None,
-             }
-    prob = NonlinearVariationalProblem(F, q, bcs=[bc1, bc2])
-    solv = NonlinearVariationalSolver(prob, solver_parameters=params)
-    solv.solve()
-    u, eta = q.split()
-    u.rename('Velocity')
-    eta.rename('Elevation')
+        # solver parameters
+        self.params = {
+                       'snes_type': 'newtonls',
+                       #'ksp_type': 'gmres',
+                       'ksp_type': 'preonly',
+                       'pc_type': 'lu',
+                       #'pc_factor_mat_solver_type': 'mumps',
+                       'mat_type': 'aij',
+                       'snes_monitor': None,
+                       'ksp_monitor': None,
+                       #'ksp_converged_reason': None,
+                      }
 
-    # plot
-    File('outputs/' + op.approach + '/sol.pvd').write(u, eta)
+        # plotting
+        self.outfile = File('outputs/' + op.approach + '/sol.pvd')
 
-    # objective functional
-    unorm = sqrt(inner(u, u))
-    J = assemble(C_D*(unorm**3)*dx)
+    def solve_onestep(self):
+        g = self.g
+        nu = self.nu
+        n = self.n
 
-    return J
+        # function spaces etc
+        u, eta = split(self.sol)
+        z, zeta = TestFunctions(self.V)
+        H = eta + self.b
+        if norm(self.sol) < 1e-8:
+            u_old = interpolate(as_vector((3., 0.)), self.V.sub(0))
+        else:
+            u_old = self.sol.split()[0].copy()
+
+        F = 0
+        # advection term
+        F += inner(z, dot(u_old, nabla_grad(u)))*dx  # note use of u_old
+        # pressure gradient term
+        F += g*dot(z, grad(eta))*dx
+        # viscosity term
+        F += -nu*inner(z, dot(n, nabla_grad(u)))*ds
+        F += nu*inner(grad(z), grad(u))*dx
+        # drag term
+        F += (self.C_D+self.C_b)*sqrt(dot(u_old, u_old))*dot(z, u)/H*dx  # note use of u_old
+        # hudiv term (in.c Neumann condition)
+        F += zeta*H*dot(u, n)*ds(1) + zeta*H*dot(u, n)*ds(2)
+        F += -H*dot(u, grad(zeta))*dx
+
+        # Dirichlet boundary conditions
+        inflow = Constant((3., 0.))
+        bc1 = DirichletBC(self.V.sub(0), inflow, 1)
+        bc2 = DirichletBC(self.V.sub(1), 0, 2)
+
+        # solve
+        prob = NonlinearVariationalProblem(F, self.sol, bcs=[bc1, bc2])
+        solv = NonlinearVariationalSolver(prob, solver_parameters=self.params)
+        solv.solve()
+        u, eta = self.sol.split()
+        u.rename('Velocity')
+        eta.rename('Elevation')
+
+        # plot
+        self.outfile.write(u, eta)
+
+    def solve(self):
+        print('Solving with assumed constant velocity')
+        self.solve_onestep()
+        print('Solving with previously established velocity')
+        self.solve_onestep()
+
+    def objective_functional(self):
+        u = self.sol.split()[0]
+        unorm = sqrt(inner(u, u))
+        self.J = assemble(self.C_D*(unorm**3)*dx)
+        print("Objective functional: {:.4e}".format(self.J))
+        return self.J
 
 
 if __name__ == "__main__":
-    mesh = Mesh('channel.msh')
-    solve_turbine(mesh)
+
+    tp = TurbineProblem()
+    tp.solve()
+    J = tp.objective_functional()

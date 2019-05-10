3e6bfc3877848f737027915314d286ed7355827f
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-05-10 09:07:10 +0100

    Extra options and uniform runs

--- steady/run.py
+++ steady/run.py
@@ -7,7 +7,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-approach", help="Choose adaptive approach")
 parser.add_argument("-dwr_approach")
-parser.add_argument("-num_turbines")
+parser.add_argument("-adapt_field")
+parser.add_argument("-restrict")
+parser.add_argument("-desired_error")
+parser.add_argument("-num_turbines", help="Choose number of turbines from 2 or 15.")
 args = parser.parse_args()
 
 num_turbines = 2 if args.num_turbines is None else int(args.num_turbines)
@@ -23,10 +26,10 @@ else:
 
 if args.dwr_approach is not None:
     op.dwr_approach = args.dwr_approach
-#op.desired_error = 1e-5
-#op.max_anisotropy = 50.
+op.restrict = 'error' if args.restrict is None else args.restrict
+op.desired_error = 1e-5 if args.desired_error is None else float(args.desired_error)
+op.adapt_field = 'fluid_speed' if args.adapt_field is None else args.adapt_field
 print(op)
-op.desired_error = 1e-2
 
 #tp = SteadyTurbineProblem(mesh=mesh, op=op)
 #tp.solve()
@@ -34,8 +37,9 @@ op.desired_error = 1e-2
 #tp.adapt_mesh()
 #tp.plot()
 
-mo = MeshOptimisation(SteadyTurbineProblem, op, mesh)
-mo.iterate()
+#mo = MeshOptimisation(SteadyTurbineProblem, op, mesh)
+#mo.iterate()
 
-#ol = OuterLoop(SteadyTurbineProblem, op, mesh)
+ol = OuterLoop(SteadyTurbineProblem, op, mesh)
 #ol.scale_to_convergence()
+ol.desired_error_loop()

151d8b6ded27a4f804073016ed2428bab6d9956a
joint test account <n.barral@imperial.ac.uk>
2019-05-17 13:00:17 +0100

    Uniform runs one by one

--- steady/uniform.py
+++ steady/uniform.py
@@ -5,33 +5,29 @@ import argparse
 
 # initialise
 parser = argparse.ArgumentParser()
-parser.add_argument("-startit", help="Starting iterations.")
-parser.add_argument("-maxit", help="Maximum number of mesh iterations.")
-parser.add_argument("-objective_rtol", help="Relative tolerance for convergence in objective.")
-parser.add_argument("-initial_mesh", help="Choose initial mesh from 'uniform', 'coarse', 'fine'")
+parser.add_argument("-resolution")
+#parser.add_argument("-initial_mesh", help="Choose initial mesh from 'uniform', 'coarse', 'fine'")
 parser.add_argument("-num_turbines")
 args = parser.parse_args()
-startit = 0 if args.startit is None else int(args.startit)
-maxit = 6 if args.maxit is None else int(args.maxit)
-objective_rtol = 0.0001 if args.objective_rtol is None else float(args.objective_rtol)
 
 num_turbines = 2 if args.num_turbines is None else int(args.num_turbines)
-initial_mesh = 'uniform' if args.initial_mesh is None else args.initial_mesh
-if initial_mesh == 'uniform':
-    mesh = None  # TODO: parallel version for 15-turbine
-elif initial_mesh =='coarse':
-    mesh = Mesh('../coarse_{:d}_turbine.msh'.format(num_turbines))  # TODO: for 15-turbine
-elif initial_mesh =='fine':
-    mesh = Mesh('../fine_{:d}_turbine.msh'.format(num_turbines))
-    raise NotImplementedError
+# TODO: MeshHierarchy for coarse/fine
+#initial_mesh = 'uniform' if args.initial_mesh is None else args.initial_mesh
+#if initial_mesh == 'uniform':
+#    mesh = None  # TODO: parallel version for 15-turbine
+#elif initial_mesh =='coarse':
+#    mesh = Mesh('../coarse_{:d}_turbine.msh'.format(num_turbines))  # TODO: for 15-turbine
+#elif initial_mesh =='fine':
+#    mesh = Mesh('../fine_{:d}_turbine.msh'.format(num_turbines))
+#else:
+#    raise NotImplementedError
+n = 0 if args.resolution is None else float(args.resolution)
+mesh = RectangleMesh(100*2**n, 20*2**n, 1000, 200)
 
 # optimise
 op = Steady2TurbineOptions(approach='uniform') if num_turbines == 2 else Steady15TurbineOptions(approach='uniform')
 op.family = 'dg-cg'
-mo = MeshOptimisation(SteadyTurbineProblem, op, mesh=mesh)
-mo.startit = startit
-mo.maxit = maxit
-mo.objective_rtol = objective_rtol
-mo.iterate()
+tp = SteadyTurbineProblem(op, mesh=mesh)
+tp.solve()
 print('Done!')
 

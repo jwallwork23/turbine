01de611f65dc914a0a090493a9c8a5e23dbec83b
Joe Wallwork <j.wallwork16@imperial.ac.uk>
2019-05-17 12:58:06 +0100

    Some argparse changes

--- steady/run.py
+++ steady/run.py
@@ -9,11 +9,13 @@ parser.add_argument("-approach", help="Choose adaptive approach")
 parser.add_argument("-dwr_approach")
 parser.add_argument("-adapt_field")
 parser.add_argument("-restrict")
-parser.add_argument("-desired_error")
+parser.add_argument("-target")
 parser.add_argument("-initial_mesh", help="Choose initial mesh from 'uniform', 'coarse', 'fine'")
 parser.add_argument("-num_turbines", help="Choose number of turbines from 2 or 15.")
 parser.add_argument("-outer_startit")
 parser.add_argument("-outer_maxit")
+parser.add_argument("-maxit")
+parser.add_argument("-objective_rtol")
 args = parser.parse_args()
 
 num_turbines = 2 if args.num_turbines is None else int(args.num_turbines)
@@ -26,13 +28,12 @@ elif initial_mesh =='coarse':
     mesh = Mesh('../coarse_{:d}_turbine.msh'.format(num_turbines))  # TODO: for 15-turbine
 elif initial_mesh =='fine':
     mesh = Mesh('../fine_{:d}_turbine.msh'.format(num_turbines))
-    raise NotImplementedError
 op = Steady2TurbineOptions(approach=approach) if num_turbines == 2 else Steady15TurbineOptions(approach=approach)
 
 if args.dwr_approach is not None:
     op.dwr_approach = args.dwr_approach
-op.restrict = 'error' if args.restrict is None else args.restrict
-op.desired_error = 1e-5 if args.desired_error is None else float(args.desired_error)
+op.restrict = 'target' if args.restrict is None else args.restrict
+op.target = 1e+5 if args.target is None else float(args.target)
 op.adapt_field = 'fluid_speed' if args.adapt_field is None else args.adapt_field
 op.family = 'dg-cg'
 print(op)
@@ -48,7 +49,9 @@ print(op)
 
 ol = OuterLoop(SteadyTurbineProblem, op, mesh)
 #ol.scale_to_convergence()
-ol.outer_startit = 0 if args.outer_startit is None else int(args.outer_startit)
-ol.outer_maxit = 4 if args.outer_maxit is None else int(args.outer_maxit)
-ol.objective_rtol = 0.001  # TODO: temp
+ol.outer_startit = 2 if args.outer_startit is None else int(args.outer_startit)
+ol.outer_maxit = 7 if args.outer_maxit is None else int(args.outer_maxit)
+ol.maxit = 35 if args.maxit is None else int(args.maxit)
+if args.objective_rtol is not None:
+    ol.objective_rtol = float(args.objective_rtol)
 ol.desired_error_loop()
